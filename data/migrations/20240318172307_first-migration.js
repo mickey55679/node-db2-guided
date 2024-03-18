96eb1506843c9130fe40c56c18cb3bf955c03392
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-18 12:32:56 -0500

    migrate is functional

--- data/migrations/20240318172307_first-migration.js
+++ data/migrations/20240318172307_first-migration.js
@@ -2,14 +2,16 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('fruits', table => {
+  table.increments('fruits_id')
+  }) // takes two arguments 
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('fruits') //we need ifExists because someone might clone the project and migrate down 
 };
